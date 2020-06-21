9242b5fbdb8ecb05f36be016d1f3990355e6f2b8
Nostros <shadowmane7@yahoo.com>
2020-06-21 03:08:20 -0400

    Created Navmesh, made player a navmesh agent, gave it a mover script, created a target object for player to follow

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    //config
+    [SerializeField] Transform target;
+
+    //cache
+    [SerializeField] NavMeshAgent myNavMeshAgent;
+
+    //Unity Functions
+    void Start()
+    {
+        CacheRefrences();
+    }
+    
+    void Update()
+    {
+        if (target != null) { myNavMeshAgent.destination = target.position; }
+    }
+
+    //Custom Functions
+    private void CacheRefrences()
+    {
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+}
