555ae8c03a3e6a7c0291c0f4ac6300e7ac2484ec
Nostros <shadowmane7@yahoo.com>
2020-06-29 01:07:45 -0400

    created follow camera

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    //cache refrences
+    [SerializeField] GameObject target;
+
+    //Unity Functions
+     void Update()
+    {
+        transform.position = target.transform.position;
+    }
+}
